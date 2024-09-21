56b219d34cab5c8f7fa022c26cc1c9d4cc51f6f8
Hamza İnce <hamzaince001@gmail.com>
2024-09-21 14:04:35 +0300

    card added swippes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,70 @@
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 5, // 5 öğe varsayılan olarak görünür
-  spaceBetween: 20, // Slaytlar arasındaki boşluk
-  breakpoints: {
-    768: {
-      slidesPerView: 5, // 768 piksel ve üstü için 5 öğe
-    },
-    320: {
-      slidesPerView: 2, // Mobil için 2 öğe göster
-    }
-  },
+const mainSwiper = new Swiper('.main-swiper-container', {
+  slidesPerView: 5,
+  spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+var gallerySliders = document.querySelectorAll('.gallery-container');
+
+gallerySliders.forEach(function (slider, index) {
+  var paginationClass = 'custom-pagination-' + index;
+
+  // Pagination container'ı bul
+  var paginationContainer = slider.querySelector('.swiper-pagination');
+  paginationContainer.classList.add(paginationClass);
+
+  // Swiper oluştur
+  var innerSlider = new Swiper(slider, {
+    loop: true,
+    autoplay: false,
+    spaceBetween: 8,
+    speed: 600,
+    grabCursor: true,
+    pagination: {
+      el: '.' + paginationClass,
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+      effect: 'slide',
+    },
+  });
+
+  // Mouse hareketini dinle
+  slider.addEventListener('mousemove', function (event) {
+    var sliderRect = slider.getBoundingClientRect();
+    var mouseX = event.clientX - sliderRect.left;
+    var sliderWidth = sliderRect.width;
+
+    // Solda mı sağda mı kontrol et
+    if (mouseX < sliderWidth / 2) {
+      if (innerSlider.autoplay.running) {
+        innerSlider.autoplay.stop();
+      }
+      innerSlider.slidePrev();
+    } else {
+      if (innerSlider.autoplay.running) {
+        innerSlider.autoplay.stop();
+      }
+      innerSlider.slideNext();
+    }
+  });
+
+  // Mouse girdiğinde autoplay başlat
+  slider.addEventListener('mouseenter', function () {
+    innerSlider.params.autoplay = { delay: 500 };
+    innerSlider.autoplay.start();
+  });
+
+  // Mouse çıktığında autoplay durdur
+  slider.addEventListener('mouseleave', function () {
+    innerSlider.autoplay.stop();
+  });
+});
+
+
+
+
